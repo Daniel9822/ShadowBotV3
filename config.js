578e7eb7bcbfe28723b6f8f94b88656bbe35d4a7
Daniel H <83887501+Daniel9822@users.noreply.github.com>
2022-03-12 18:27:50 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://c
 global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
 global.owner = ['17722386341'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
 global.mods = ['5219992095479'] // No tiene utilidad
-global.prems = ['5219996125657' , '5219991402134'] // No tiene utilidad
+global.prems = ['5219994125657' , '5219991402134'] // No tiene utilidad
 
 //Lo siguiente no tiene nada que ver con el funcionamiento del Bot:
 // Nota: Puedes contactarme si necesitas ayuda con algo al +5219996125657 (este es mi numero personal/real bloqueo personas que no sean temas de la instalación)
